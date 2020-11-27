6fdd7e8c9372cfc21d4eac0a3c326a7cf3091ddb
jhonathanmusa <jonathansm891@gmail.com>
2020-11-26 22:17:48 -0500

    thirth commit

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 const getPokemons = async () => {
   try {
-    const section = "pokemon-color";
+    const section = "pokemon";
     const res = await fetch(`https://pokeapi.co/api/v2/${section}`);
     const data = await res.json();
     const singleElements = data.results.map((poke) => poke);
-    console.log(singleElements);
+    // console.log(singleElements);
+    document.getElementById("root").innerHTML = singleElements[0].name
   } catch (error) {
     console.log(error);
   }
