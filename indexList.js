ac49936862d84ffe1f139c1d33c9b6cf924265df
jhonathanmusa <jhonathansamu91@gmail.com>
2020-12-27 13:51:35 -0500

    adding filter

--- indexList.js
+++ indexList.js
@@ -1,4 +1,4 @@
-const displyaPokemon = document.querySelector(".grid-container")
+/* const displyaPokemon = document.querySelector(".grid-container")
 
 const pokemonList = async () => {
   try {
@@ -23,4 +23,52 @@ const pokemonList = async () => {
   }
 }
 
-pokemonList()
+pokemonList() */
+
+const btnSearch = document.getElementById("btn-search");
+const display = document.querySelector(".grid-container");
+let pokemons = [];
+
+const getPokemon = async () => {
+  try {
+    const res = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20"
+    );
+    const data = await res.json();
+    const result = data.results;
+    for (poke in result) {
+      pokemons.push(result[poke].name);
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+getPokemon();
+
+function getNames(e) {
+  let value = document.querySelector(".result").value;
+  let result = [];
+
+  compare(value, result);
+  e.preventDefault();
+}
+
+function compare(value, result) {
+  for (poke of pokemons) {
+    if (poke.includes(value)) {
+      result.push(poke);
+    }
+  }
+
+  for (poke in result) {
+    let name = result[poke];
+    let htmlCode = `
+          <div class="grid-item name">${name}</div>
+          `;
+
+    display.innerHTML += htmlCode;
+  }
+}
+
+btnSearch.addEventListener("click", getNames);
